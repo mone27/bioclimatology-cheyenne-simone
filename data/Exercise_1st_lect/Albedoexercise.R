35531b67dfed7c115d3218e449b5c2155c31d74c
CheyenneRL <c.ruedalagasse@stud.uni-goettingen.de>
2021-04-30 16:35:17 +0200

    i started

--- data/Exercise_1st_lect/Albedoexercise.R
+++ data/Exercise_1st_lect/Albedoexercise.R
@@ -13,20 +13,19 @@ conver_incoming <- calib_factorsdata$Shortwave_incoming_mV
 conver_outgoing <-  calib_factorsdata$Shortwave_outgoing_mV
 conver_belowcanopy <- calib_factorsdata$Shortwave_incoming_bewlow_canopy_mV
 
-conversion1 <- as.numeric(conver_incoming* 86.5801) #mV *( W m-2 )/mV  = W m-2
-conversion2 <- as.numeric(conver_outgoing * 86.5801) #mV *( W m-2 )/mV = W m-2
-conversion3 <- as.numeric(conver_belowcanopy * 194.9318) #mV *( W m-2 )/mV = W m-2
+incomingradiation <- as.numeric(conver_incoming* 86.5801) #mV *( W m-2 )/mV  = W m-2
+outgoingradiation <- as.numeric(conver_outgoing * 86.5801) #mV *( W m-2 )/mV = W m-2
+radiationbelowcanopy<- as.numeric(conver_belowcanopy * 194.9318) #mV *( W m-2 )/mV = W m-2
 
-dataconverted <- c("conversion1, conversion2,conversion3")
-View(dataconverted)
-albedo <- conversion1-conversion2
-albedo_canopy <- conversion2-conversion3
-plot(albedo, ylim = c(-700,850 ),main='Albedo',
+
+albedo <- outgoingradiation/incomingradiation
+albedo_canopy <- outgoingradiation/radiationbelowcanopy
+plot(albedo, ylim = c(0,3500 ),main='Albedo',
      xlab='', ylab='', col="blue", type = "l" )
 lines(albedo_canopy, col="red")
 legend("bottomright", inset=0.01, legend=c("albedo", "albedo with canopy"), 
        lwd=2, cex=1.2, col=c("blue", "red"), bg="lightyellow")
 
 totalalbedo <- albedo-albedo_canopy
-plot(totalalbedo, ylim = c(-250,1100 ),main='Albedo',
+plot(totalalbedo,main='Albedo',
      xlab='', ylab='', col="blue", type = "l" )
---
+++ data/Longwave_Exercise2nd_lect/2ndexercise.R
@@ -0,0 +1,35 @@
+
+data <- read.csv("LW_SW_TSoil_BotGarten.csv")
+View(data)
+A <- data$LW_IN_W_m.2 #atmospheric longwave radiation
+E <- data$LW_OUT_W_m.2 #surface reflected longwave radiation
+netbalance_lw <- A-E
+
+Rsw <- data$SW_OUT_W_m.2 #Reflected solar radiation
+G <-data$SW_IN_W_m.2 #Global radiation, is the sum of direct and diffuse solar radiation
+netbalance_sw <- Rsw/G
+
+netradiationbalance_earthsurface <- netbalance_lw+netbalance_sw
+
+sky_temp <- 
+surface_temp <- 
+
+#Derive the sky and surface temperature from the longwave radiation
+#components. How do they differ and why? Discuss! During which
+#periods of the year sky and surface temperature differ the most and
+#the less?
+
+##1a. 
+##1b. I think in winter, due to the heat storaged from earth during the whole year and the less intense radiation from the sun coming during this season
+  
+#Compare the soil surface temperature derived from the longwave
+#radiation with the soil temperature at 0.02 m depth. To which degree
+#can the outgoing longwave radiation be used as an estimate of
+#surface temperature and what are potential errors?
+
+##2a. 
+
+#Calculate the net radiation over the meadow in the forest botanical
+#garden and plot the four components. How do the four components
+#change over the season and why? Which unexpected results you
+#found? Discuss!
