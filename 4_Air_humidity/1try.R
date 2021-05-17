bacba16a2759c5493730046153818529c7517617
CheyenneRL <c.ruedalagasse@stud.uni-goettingen.de>
2021-05-17 09:04:21 +0200

    sdsd

---
+++ 4_Air_humidity/1try.R
@@ -0,0 +1,25 @@
+#Calculate and visualize
+
+data <- read.csv("04_Air_humidity")
+#1. actual vapor pressure,
+
+#????v <- density of water vapor
+Rv <- R/Mv = 461.47 #[J K-1 Kg -1] gas constant of water vapor
+R <- 8.314 J K-1 #universal gas constant
+Mv <-  18.016 *10-3 #Kg mol-1
+
+pH2O = ea = ????v Rv T #[hPa]
+
+pH20 <- 
+#2. saturation vapor pressure,
+
+#3. dewpoint temperature,
+#4. absolute humidity,
+#5. specific humidity,
+#6. mixing ratio,
+#7. vapor pressure deficit, and equivalent temperature.
+#-> How and when do actual and saturation vapour pressure differ?
+ # -> Compare air and dew point temperature, how do they differ and how are they related to relative humidity?
+ # -> When is absolute humidity largest and why?
+ # -> Compare mixing ratio and specific humidity, how and why do they differ?
+  #-> When is the vapour pressure deficit and equivalent temperature largest?
