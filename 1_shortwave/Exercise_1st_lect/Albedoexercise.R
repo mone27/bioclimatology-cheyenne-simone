bc4df5a3c43b5ab44ae13cb5dab69eec6503997e
CheyenneRL <c.ruedalagasse@stud.uni-goettingen.de>
2021-05-18 11:59:27 +0200

    need some back up to finish the point 1.4 of the first protocol

--- 1_shortwave/Exercise_1st_lect/Albedoexercise.R
+++ 1_shortwave/Exercise_1st_lect/Albedoexercise.R
@@ -1,6 +1,7 @@
 Shortwavedata <- read.csv("Shortwave_incoming_diffuse_W_m-2_Hainich_2020.csv", header = TRUE, sep = ",")
 View(Shortwavedata)
 incoming_diffuse <- Shortwavedata$Shortwave_incoming_diffuse_W_m.2
+
 incoming_directly <- Shortwavedata$Shortwave_incoming_W_m.2
 
 
@@ -9,17 +10,13 @@ incoming_directly <- Shortwavedata$Shortwave_incoming_W_m.2
 calib_factorsdata <- read.csv("Shortwave_incoming_outgoing_belowcanopy_mV_Hainich_2020.csv")
 
 View(calib_factorsdata)
-conver_incoming <- calib_factorsdata$Shortwave_incoming_mV
-conver_outgoing <-  calib_factorsdata$Shortwave_outgoing_mV
-conver_belowcanopy <- calib_factorsdata$Shortwave_incoming_bewlow_canopy_mV
-
-incomingradiation <- as.numeric(conver_incoming* 86.5801) #mV *( W m-2 )/mV  = W m-2
-outgoingradiation <- as.numeric(conver_outgoing * 86.5801) #mV *( W m-2 )/mV = W m-2
-radiationbelowcanopy<- as.numeric(conver_belowcanopy * 194.9318) #mV *( W m-2 )/mV = W m-2
+conver_incoming <- calib_factorsdata$Shortwave_incoming_mV * 86.5801 #mV *( W m-2 )/mV  = W m-2
+conver_outgoing <-  calib_factorsdata$Shortwave_outgoing_mV * 86.5801 #mV *( W m-2 )/mV  = W m-2
+conver_belowcanopy <- calib_factorsdata$Shortwave_incoming_bewlow_canopy_mV*194.9318 #mV *( W m-2 )/mV = W m-2
 
 
-albedo <- outgoingradiation/incomingradiation
-albedo_canopy <- outgoingradiation/radiationbelowcanopy
+albedo <- conver_outgoing/conver_incoming
+albedo_canopy <- conver_outgoing/conver_belowcanopy
 plot(albedo, ylim = c(0,3500 ),main='Albedo',
      xlab='', ylab='', col="blue", type = "l" )
 lines(albedo_canopy, col="red")
